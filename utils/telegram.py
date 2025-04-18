b3a88f67dd933980e16e20e6796a9d67f9c8cfab
maslovmaksim92 <vashdom.kaluga@gmail.com>
2025-04-18 23:31:56 +0300

    feat: добавлена отправка одного сообщения в Telegram с фото и подписью

---
+++ utils/telegram.py
@@ -0,0 +1,22 @@
+import os
+import httpx
+from loguru import logger
+
+TG_BOT_TOKEN = os.getenv("TG_GITHUB_BOT")
+TG_CHAT_ID = os.getenv("TG_CHAT_ID")
+
+TELEGRAM_API = f"https://api.telegram.org/bot{TG_BOT_TOKEN}/sendPhoto"
+
+
+async def send_photo_to_telegram(image_url: str, address: str):
+    text = f"\U0001F9F9 Уборка подъездов завершена\n\U0001F3E0 Адрес: {address}\n\U0001F4C5 Дата: сегодня"
+    logger.info(f"\U0001F4F7 Отправка в Telegram: {image_url}")
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            TELEGRAM_API,
+            data={"chat_id": TG_CHAT_ID, "caption": text, "photo": image_url},
+        )
+    if not response.status_code == 200:
+        logger.warning(f"❌ Telegram error: {response.text}")
+    else:
+        logger.info("✅ Сообщение отправлено в Telegram")
