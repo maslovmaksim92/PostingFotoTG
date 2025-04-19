1715b171f0341ace138292b0ca3845a31807c6a7
maslovmaksim92 <vashdom.kaluga@gmail.com>
2025-04-19 16:19:23 +0300

    🔧 Добавлены недостающие поля Bitrix в pydantic Settings: BITRIX_CLIENT_ID, BITRIX_CLIENT_SECRET, FILE_FIELD_ID, FOLDER_FIELD_ID

--- config.py
+++ config.py
@@ -1,13 +1,21 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    OPENAI_API_KEY: str
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
-    model_config = {
-        "env_file": ".env",
-        "extra": "ignore"
-    }
+    OPENAI_API_KEY: str
+    TG_CHAT_ID: str
+    TG_GITHUB_BOT: str
+
+    BASIC_AUTH_LOGIN: str
+    BASIC_AUTH_PASSWORD: str
+
+    BITRIX_CLIENT_ID: str
+    BITRIX_CLIENT_SECRET: str
+
+    FILE_FIELD_ID: str
+    FOLDER_FIELD_ID: str
 
 
 settings = Settings()
