62e2d36990076ba3aa434f745fec538327091f12
maslovmaksim92 <vashdom.kaluga@gmail.com>
2025-04-20 00:57:55 +0300

    revert: возвращён импорт send_media_group из utils.telegram_client для совместимости

--- src/senders/telegram.py
+++ src/senders/telegram.py
@@ -1,9 +1,8 @@
-from telegram_sender import send_telegram_media_group
+from utils.telegram_client import send_media_group
 
-# legacy function for compatibility
+# старый интерфейс
 async def send_message(chat_id: str, text: str):
-    pass  # старый интерфейс, пока оставим
+    pass
 
-# прокси к новой функции
-async def send_telegram_group(chat_id: str, files):
-    await send_telegram_media_group(chat_id=chat_id, files=files)
+async def send_telegram_media_group(chat_id: str, files):
+    await send_media_group(chat_id, files)
