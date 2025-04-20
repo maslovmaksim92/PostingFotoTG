7ef8d5db999816c2651325a521fa1ccf37335dd3
maslovmaksim92 <vashdom.kaluga@gmail.com>
2025-04-20 14:17:26 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -39,3 +39,14 @@ def send_video(video: bytes, caption: str):
     response = requests.post(url, data=data, files=files)
     response.raise_for_status()
     logger.info("Видео отправлено")
+
+def send_log_to_telegram(text: str):
+    url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
+    data = {
+        "chat_id": TG_CHAT_ID,
+        "text": text,
+        "parse_mode": "Markdown"
+    }
+    response = requests.post(url, data=data)
+    response.raise_for_status()
+    logger.debug("Лог отправлен в Telegram")
