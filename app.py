253bf2e9ce9e4376e8053e430d53d71dd2e63a8d
maslovmaksim92 <vashdom.kaluga@gmail.com>
2025-04-26 09:01:09 +0300

    feat: подключен debug/files_summary.py как /debug/files

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from webhook import router as webhook_router
+from debug.files_summary import router as debug_router
 
 app = FastAPI()
+
 app.include_router(webhook_router)
+app.include_router(debug_router)  # Добавлен debug endpoint
