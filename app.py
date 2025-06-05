bed2b16ad019aca50a3958272e90231e1960f357
maslovmaksim92 <vashdom.kaluga@gmail.com>
2025-06-05 11:40:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from webhook import router as webhook_router
 from loguru import logger
+from agent_bot.handler import start_agent_bot
+
+@app.on_event("startup")
+async def startup():
+    asyncio.create_task(start_agent_bot())
 
 # Явно создаём приложение
 app = FastAPI()
